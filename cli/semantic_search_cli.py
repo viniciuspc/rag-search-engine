32d0cba8ad1dd79c71867426399c1fd7e1b3c7d0
Vinicius Carvalho <vinicius.carvalho@fraunhofer.pt>
2026-05-13 12:30:40 +0100

    Add SemanticSearch class to load embedings models.

--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
 
 import argparse
+from lib.semantic_search import verify_model
 
 def main():
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
+    
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+    
+    subparsers.add_parser("verify", help="Verify the model for semantic search")
+    
     args = parser.parse_args()
 
     match args.command:
+        case "verify":
+            verify_model()
         case _:
             parser.print_help()
 
